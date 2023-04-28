cb1819f321804d60081e08f1075072a50befe839
githubMYL <feverdy86@gmail.com>
2023-04-28 17:44:56 +0900

    Mozip

--- src/test/java/org/mozip/tests/MozipTest.java
+++ src/test/java/org/mozip/tests/MozipTest.java
@@ -1,9 +1,16 @@
 package org.mozip.tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Mozip 통합테스트")
 
 public class MozipTest {
     private static final String DRIVER = "oracle.jdbc.OracleDriver"; //Connection 을 구현한 클래스의 이름
@@ -13,12 +20,12 @@ public class MozipTest {
 
     @Test //jUnit이 테스트함
     public void testConnection() throws Exception{
-        Class.forName(DRIVER); //DRIVER라는 이름을 가진 클래스를 찾음
+        Class.forName(DRIVER); //DRIVER 라는 이름을 가진 클래스를 찾음
 
-        //DB 계정과 연결된 객체를 Connection 클래스의 인스턴스인 con에 담음
+        //DB 계정과 연결된 객체를 Connection 클래스의 인스턴스인 con 에 담음
         try(Connection con = DriverManager.getConnection(URL,USER,PW)){
-            System.out.println(con); //연결된 계정 출력
-        }catch(Exception e) { //연결이 되지 않은 예외처리
+            System.out.println(con);    //연결된 계정 출력
+        }catch(Exception e) {           //연결이 안되면 예외처리
             e.printStackTrace();
         }
     }
