100ec154dbe2b77c9bd0da0df606af4b6e96090e
changyoung4087 <double4087@naver.com>
2023-04-30 13:06:21 +0900

    Mozip park 230430

--- src/main/java/org/mozip/configs/MvcConfig.java
+++ src/main/java/org/mozip/configs/MvcConfig.java
@@ -1,9 +1,13 @@
 package org.mozip.configs;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableJpaAuditing
+@RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
 
 }
