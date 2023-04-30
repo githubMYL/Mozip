660eb744ebaf1aff3b3bd9030b504fd86fb435a4
MYL <feverdy86@gmail.com>
2023-04-30 12:41:45 +0900

    Mozip

--- src/main/java/org/mozip/configs/MvcConfig.java
+++
@@ -1,9 +0,0 @@
-package org.mozip.configs;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class MvcConfig implements WebMvcConfigurer {
-
-}
