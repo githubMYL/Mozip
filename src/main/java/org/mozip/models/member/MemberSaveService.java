8f82e65450e96b8647b5991137e2cae33582b79d
changyoung4087 <double4087@naver.com>
2023-05-01 14:07:40 +0900

    Mozip 230501

--- src/main/java/org/mozip/models/member/MemberSaveService.java
+++ src/main/java/org/mozip/models/member/MemberSaveService.java
@@ -4,19 +4,20 @@ import lombok.RequiredArgsConstructor;
 import org.mozip.controllers.entities.Members;
 import org.mozip.controllers.members.JoinParam;
 import org.mozip.repositories.MembersRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberSaveService {
     private final MembersRepository repository;
-    //private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     public void save(JoinParam joinParam){
         Members member = JoinParam.of(joinParam);
 
-//        String hash = passwordEncoder.encode(joinParam.getMemberPw());
-//        member.setMemberPw(hash);
+        String hash = passwordEncoder.encode(joinParam.getMemberPw());
+        member.setMemberPw(hash);
 
         repository.saveAndFlush(member);
     }
