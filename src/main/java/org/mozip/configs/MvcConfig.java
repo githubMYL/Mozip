042a78d533d436c0044111507924b5d1656aa3e0
changyoung4087 <double4087@naver.com>
2023-05-01 14:13:33 +0900

    커밋 충돌 해결

--- src/main/java/org/mozip/configs/MvcConfig.java
+++ src/main/java/org/mozip/configs/MvcConfig.java
@@ -14,20 +14,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     @Bean
-<<<<<<< HEAD
-    public MessageSource messageSource(){
-        // 메시지관련 메서드
-=======
     public MessageSource messageSource() {
->>>>>>> dc145500e999787e62475910dc791f05b19a038c
+
         ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
         ms.setDefaultEncoding("UTF-8");
         ms.addBasenames("messages.commons", "messages.errors", "messages.validations");
         return ms;
     }
-<<<<<<< HEAD
-}
-=======
+
 
 }
->>>>>>> dc145500e999787e62475910dc791f05b19a038c
+
