261407997c6836b642ac014a60053eddcfa90340
yonggyo1125 <yonggyo00@kakao.com>
2023-05-08 08:59:23 +0900

    수정 상황 반영

--- src/main/java/org/mozip/models/mozip/MozipListService.java
+++ src/main/java/org/mozip/models/mozip/MozipListService.java
@@ -1,13 +1,21 @@
 package org.mozip.models.mozip;
 
+import lombok.RequiredArgsConstructor;
 import org.mozip.controllers.entities.Mozip;
 import org.mozip.controllers.mypage2.MozipSearch;
+import org.mozip.repositories.MozipRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MozipListService {
+
+    private final MozipRepository mozipRepository;
+
     public Page<Mozip> gets(MozipSearch search) {
+        
+
         return null;
     }
 }
