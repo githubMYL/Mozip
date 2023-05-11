25cf2956949e526d6c8586fdfa7b9572148a51b2
ParkDogeon <bdg3126@gmail.com>
2023-05-11 15:46:14 +0900

    css/html 수정 0511

--- src/main/java/org/mozip/controllers/main/MainController.java
+++ src/main/java/org/mozip/controllers/main/MainController.java
@@ -1,19 +1,26 @@
 package org.mozip.controllers.main;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.mozip.commons.MemberUtil;
+import org.mozip.entities.Mozip;
 import org.mozip.models.member.MemberInfo;
+import org.mozip.models.mozip.MozipInfoService;
+import org.mozip.repositories.MozipRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/mozip")
-@Log
+@RequiredArgsConstructor
 public class MainController {
 
+    private final MozipRepository repository;
     @Autowired
     private MemberUtil memberUtil;
     @GetMapping
@@ -22,12 +29,14 @@ public class MainController {
         MemberInfo memberInfo = memberUtil.getMember();
         if(memberInfo != null){
             String memberNick = memberInfo.getMemberNick();
-            System.out.println("memberNick ::::::::::::: " + memberInfo.getMemberNick());
             model.addAttribute("memberNick", memberNick);
         }
         model.addAttribute("addCss", new String[] {"main/style"});
 
-        return "mozip";
+        List<Mozip> mozips = repository.findAll();
+        System.out.println("mozips ::::::::::::::::::: >> " + mozips);
+        model.addAttribute("mozips", mozips);
 
+        return "mozip";
     }
 }
