45328bc35135c67208ce252c8331ae0a0f3d1667
minininoi <aosend23@naver.com>
2023-05-11 12:15:17 +0900

    mypage

--- src/main/java/org/mozip/controllers/mypage/MyPageController.java
+++ src/main/java/org/mozip/controllers/mypage/MyPageController.java
@@ -1,13 +1,14 @@
 package org.mozip.controllers.mypage;
 import groovy.util.logging.Log;
 import lombok.RequiredArgsConstructor;
+import org.mozip.models.file.FileListService;
 import org.mozip.models.member.MemberInfo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import java.nio.file.Paths;
+
 
 
 @Controller
@@ -17,6 +18,7 @@ import java.nio.file.Paths;
 public class MyPageController {
     @Value("${fileupload.path}")
     private String fileUploadPath;
+    private FileListService listService;
 
     @GetMapping("/{memberNick}") //마이페이지->회원명
     public String myInfo(Model model){
@@ -25,10 +27,8 @@ public class MyPageController {
         MemberInfo member = (MemberInfo) principal;
         System.out.println("userInfo : " + member);
 
-        model.addAttribute("profilePath", Paths.get(fileUploadPath.replace("**",fileUploadPath).toString()));
-        model.addAttribute("member",member);
-
         return "mypage/index";
     }
+
 
 }
